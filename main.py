efadd1d2730f700d28bd95664550c67fa761c70c
brylimz <superuser@brylimz.com>
2021-12-16 21:49:14 +0700

    background changed

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx = 100, pady=50)
+window.config(padx = 100, pady=50, bg=YELLOW)
 
 canvas = Canvas(width=200, height=224)
 tomato_img = PhotoImage(file="tomato.png")
