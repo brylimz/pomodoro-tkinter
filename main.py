6c77e46d3b80ccad6a0148b06e787f34eba0e28d
brylimz <superuser@brylimz.com>
2021-12-16 21:53:26 +0700

    background changed

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx = 100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(103, 112, image=tomato_img)
 canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
